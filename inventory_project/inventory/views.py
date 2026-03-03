843ace9e39a99a9bdf72a939e643127d3eba5759
Nikhilesh-Satish <nikhilesh1904@gmail.com>
2026-03-04 00:29:36 +0530

    Implemented API using Router and basic authentication

---
+++ inventory_project/inventory/views.py
@@ -0,0 +1,25 @@
+
+# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Product
+from .serializers import ProductSerializer
+
+## @api_view(['GET'])
+##def product_list(request):
+  ##  products=Product.objects.all()
+    ##serializer=ProductSerializer(products,many=True)
+    ##return Response(serializer.data)
+
+##@api_view(['GET'])
+##ef api_root(request):
+  ##  return Response({
+    ##    "message":"Inventory Management API",
+      ##  "available_endpoints":{
+        ##    "products":"/api/products/"
+       ## }
+    ##})
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    permission_classes=[IsAuthenticated]
